811f1ce41106a85c0acac4c656c77a7a9ad9ca09
PapiCLY <christianyehudah@gmail.com>
2024-10-23 20:05:33 -0400

    header created

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req,res)=>{
+    res.render('index.ejs')
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on port: ${PORT}`)
 })
