aa53af9be0ec1679521ca68d87cc44b52320ada9
PapiCLY <christianyehudah@gmail.com>
2024-10-24 00:32:47 -0400

    routes working

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get('/', (req,res)=>{
     res.render('index.ejs')
 })
 
+app.get('/blogs', (req,res)=>{
+    res.render('blogs.ejs')
+})
+
+app.get('/about', (req,res)=>{
+    res.render('about.ejs')
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on port: ${PORT}`)
 })
