7e9bc7eb3f2bc16b6d4fce14f773097f457fb197
PapiCLY <christianyehudah@gmail.com>
2024-10-24 01:10:26 -0400

    slight issue with post route

--- index.js
+++ index.js
@@ -12,23 +12,29 @@ app.get('/', (req,res)=>{
 })
 
 app.get('/blogs', (req,res)=>{
-    res.render('blogs.ejs')
+    res.render('blogs.ejs', {posts: blogPosts})
 })
 
 app.get('/about', (req,res)=>{
     res.render('about.ejs')
 })
 
+let blogPosts = []
+
 app.post('/submit', (req,res)=>{
     const now = new Date()
     const readableDateTime = now.toLocaleString();
-    const data = {
+    const newPost = {
         bloggerName: req.body['bloggerName'],
         postTitle: req.body['title'],
         bloggerPost: req.body['blogPost'],
         timeStamp: readableDateTime
     }
-    res.render('blogs.ejs', data)
+
+    blogPosts.push(newPost)
+    res.render('blogs.ejs', newPost)
+
+    res.render('blogs.ejs', {posts:blogPosts})
 })
 
 app.listen(PORT, ()=>{
