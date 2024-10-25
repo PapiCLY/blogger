de6d8f66ecc9de06bd0c12dd363d5908fbf4c081
PapiCLY <christianyehudah@gmail.com>
2024-10-25 02:07:56 -0400

    added dotenv

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import 'dotenv/config';
+
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
